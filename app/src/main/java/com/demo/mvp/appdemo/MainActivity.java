e02262144f655ef837b0151443b108f3086cd164
Jorge Atempa <atempaitm@gmail.com>
2018-10-23 22:34:13 -0700

    Obtener datos a partir de un evento de desplazamiento en pantalla

--- app/src/main/java/com/demo/mvp/appdemo/MainActivity.java
+++ app/src/main/java/com/demo/mvp/appdemo/MainActivity.java
@@ -1,5 +1,6 @@
 package com.demo.mvp.appdemo;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -19,43 +20,81 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    private final String TAG = "APP_DEMO";
+    private final String TAG = "POKEDEX";
+    private Retrofit retrofit;
+    private RecyclerView recyclerView;
+    private PokemonAdapter adapter;
+    private int offset = 0;
+    private final int MAX = 20;
+    private boolean readyToLoad = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = findViewById(R.id.my_recycler_view);
-        final PokemonAdapter adapter = new PokemonAdapter(this);
+        recyclerView = findViewById(R.id.my_recycler_view);
+        adapter = new PokemonAdapter(this);
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
 
         final GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
+
         recyclerView.setLayoutManager(layoutManager);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                if (dy > 0) {
+                    int visibleItemCount = layoutManager.getChildCount();
+                    int totalItemCount = layoutManager.getItemCount();
+                    int pastVisibleItems = layoutManager.findFirstVisibleItemPosition();
+
+                    if (readyToLoad) {
+                        if ((visibleItemCount + pastVisibleItems) >= totalItemCount) {
+                            Log.i(TAG, "Llegamos al final.");
+
+                            readyToLoad = false;
+                            offset += MAX;
+                            fetchData(offset);
+                        }
+                    }
+                }
+            }
+        });
 
         // Access to API
-        Retrofit retrofit = new Retrofit.Builder()
+        retrofit = new Retrofit.Builder()
                 .baseUrl("http://pokeapi.co/api/v2/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
+        fetchData(offset);
+    }
+
+    private void fetchData(int offset) {
         ApiService service = retrofit.create(ApiService.class);
+        Call<PokemonResponse> responseCall = service.fetchPokemons(MAX, offset);
 
-        Call<PokemonResponse> res = service.fetchPokemons();
-
-        res.enqueue(new Callback<PokemonResponse>() {
+        responseCall.enqueue(new Callback<PokemonResponse>() {
             @Override
             public void onResponse(Call<PokemonResponse> call, Response<PokemonResponse> response) {
-                assert response.body() != null;
-                ArrayList<Pokemon> pokemons = response.body().getResults();
+                readyToLoad = true;
+                if (response.isSuccessful()) {
+                    assert response.body() != null;
+                    ArrayList<Pokemon> pokemons = response.body().getResults();
 
-                adapter.addPokemonList(pokemons);
+                    adapter.addPokemonList(pokemons);
+                } else {
+                    Log.e(TAG, " onResponse: " + response.errorBody());
+                }
             }
 
             @Override
             public void onFailure(Call<PokemonResponse> call, Throwable t) {
-                Log.e(TAG, "ERROR");
+                readyToLoad = true;
+                Log.e(TAG, " onFailure: " + t.getMessage());
             }
         });
     }
